a50e2303124d24db49a97cf2fed370493766de3d
10497779 <10497779@uvu.edu>
2024-07-26 14:52:16 -0600

    Returning initial instruction

--- IH.py
+++ IH.py
@@ -1,5 +1,6 @@
 class InstructionHandler4:
     def __init__(self, instruction):
+        self.instruction = instruction[1:5]
         self.operation = abs(int(instruction[1:3]))
         self.operand = int(instruction[3:5])
 
@@ -8,8 +9,9 @@ class InstructionHandler4:
 
 class InstructionHandler6:
     def __init__(self, instruction):
+        self.instruction = instruction[1:7]
         self.operation = abs(int(instruction[1:4]))
         self.operand = int(instruction[5:7])
         
     def parse(self):
-        return [self.operation, self.operand]
+        return [self.operation, self.operand, self.instruction]
